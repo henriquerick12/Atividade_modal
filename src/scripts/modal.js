d3838b4d8aecc4548378b07d5f9e140c20001d69
Henrique_Rick <henriquewood3@gmail.com>
2023-11-19 23:59:23 -0300

    Script de abrir e fechar feito

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,14 @@
 /* Desenvolva seu código aqui... */
+const modalController = document.querySelector('#modalController');
+const buttonOpenModal = document.querySelector('.form__register-link')
+const buttonClosed = document.querySelector('.modal__buttons button:first-child')
+
+buttonOpenModal.addEventListener('click', () => {
+    modalController.showModal()
+})
+
+buttonClosed.addEventListener('click', () => {
+    modalController.close()
+})
+
+
